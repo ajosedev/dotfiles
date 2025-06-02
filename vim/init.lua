e8e35e5fb7b85ebe4e495d26ab7b9d0f7cc8acbc
Andrew Jose <ajosedev@gmail.com>
2025-06-02 10:46:30 +1000

    add vim-cool

--- vim/init.lua
+++ vim/init.lua
@@ -12,14 +12,20 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
+        "romainl/vim-cool",
+        event = "VeryLazy"
+    }
 }, opts)
 
 vim.cmd([[
